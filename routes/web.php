9eb4d7078092200a8767fdc56ccd2d1d69b8bb06
Serigne Mbaye Thioub <serignembayethioub@esp.sn>
2026-04-17 15:42:10 +0000

    fix(dashboard): correction de la redirection après inscription

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ Route::get('/', [ArticleController::class, 'index'])->name('articles.index');
 Route::get('/articles/{article}', [ArticleController::class, 'show'])->name('articles.show');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', function () { return redirect()->route('articles.index');})->name('dashboard');
     Route::get('/articles/create', [ArticleController::class, 'create'])->name('articles.create');
     Route::post('/articles', [ArticleController::class, 'store'])->name('articles.store');
     Route::get('/articles/{article}/edit', [ArticleController::class, 'edit'])->name('articles.edit');
